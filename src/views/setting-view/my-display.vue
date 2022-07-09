b7bcbaab9cb7c265ccd20258134cbce75c52a537
AsheOne <1029129827@qq.com>
2022-07-09 20:10:11 +0800

    Update my-display.vue

--- src/views/setting-view/my-display.vue
+++ src/views/setting-view/my-display.vue
@@ -1,6 +1,38 @@
 <template>
-  <div>外观</div>
+  <div>
+    <div class="text-lg mb-5">外观</div>
+    <div class="h-[1px] my-6 bg-gray-500"></div>
+    <div>
+      <h5 class="mb-2 text-xs">主题</h5>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
-<style scoped></style>
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import { useUserStore } from '@/store/user'
+
+const userStore = useUserStore()
+const showSave = ref(false)
+const pickColor = ref('')
+watch(pickColor, (newColor) => {
+  if (newColor) {
+    showSave.value = true
+  }
+})
+</script>
+<style scoped>
+.slide-fade-enter-active {
+  transition: all 0.2s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.2s ease-out;
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateY(20px);
+  opacity: 0;
+}
+</style>
