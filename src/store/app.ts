e37bc44326eebac20cd9ecd817927835278ee846
宵宫 <1029129827@qq.com>
2022-08-30 21:52:13 +0800

    Update app.ts

--- src/store/app.ts
+++ src/store/app.ts
@@ -4,6 +4,7 @@ import { defineStore } from 'pinia'
 export const useAppStore = defineStore('RECORD_PINIA_APP', {
   state: () => ({
     addGuildVisible: false,
-    currentRoom: {} as RoomModel,
+    currentRoom: {},
   }),
+  persist: true,
 })
